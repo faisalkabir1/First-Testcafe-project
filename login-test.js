0bb0474d666a580fc67107f4a2f3682a675ced8b
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-02-21 23:27:44 +0600

    search testing

--- login-test.js
+++ login-test.js
@@ -3,7 +3,16 @@ import { Selector } from 'testcafe'
 fixture`Getting Started`
     .page`http://zero.webappsecurity.com/`
 
+test('User can search for a product', async t => {
+    const searchInput = Selector('#searchTerm');
 
+    const resultTitle = Selector('.result-title');
+
+    await t
+        .typeText(searchInput, 'product name')
+        .click(searchButton)
+        .expect(resultTitle.innerText).contains('product name');
+})
 test('Valid Login test', async t => {
 
 });
