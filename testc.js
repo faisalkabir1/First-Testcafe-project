df35760f8f58b73e6f5ca4795cde648196c8462b
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-02-22 00:20:51 +0600

    fixed sign in code

--- testc.js
+++ testc.js
@@ -14,5 +14,12 @@ test('User can search for a product', async t => {
     await t.expect(resultTitle.innerText).contains('online bank');
 })
 test('Valid Login test', async t => {
+    const usernameInput = Selector('#user_login');
+    const passwordInput = Selector('#user_password');
+    const submitButton = Selector('input[type="submit"][name="submit"][value="Sign in"][class="btn btn-primary"][tabindex="4"]');
 
+    await t.click("#signin_button");
+    await t.typeText(usernameInput, 'username');
+    await t.typeText(passwordInput, 'password');
+    await t.click(submitButton);
 });
